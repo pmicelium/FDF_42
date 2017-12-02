ce7e06ed980132ddfb73c28192bbfc76c3febf31
Pierre MICELI <pmiceli@e1r6p17.42.fr>
2017-12-02 20:27:49 +0100

    premier jet

---
+++ fdf/libft/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmiceli <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 23:23:06 by pmiceli           #+#    #+#             */
+/*   Updated: 2017/11/10 11:11:32 by pmiceli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strcmp(char const *s1, char const *s2)
+{
+	int		i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	if (i != 0)
+		i--;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
